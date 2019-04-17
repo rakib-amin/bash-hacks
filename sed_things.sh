ca08b5bd3ec0e412257b073b7412ed982a2235a2
Mohammad Rakib Amin <rakib-amin@users.noreply.github.com>
2019-04-17 15:04:40 +0800

    Update sed_things.sh

--- sed_things.sh
+++ sed_things.sh
@@ -1,2 +1,5 @@
 # (macOS) duplicate a line (e.g line no 6), change the duplicate
 sed -i '' -e "6s/^//p; 6s/^.*/replacement_text_goes_here/" file.txt
+
+# (macOS) delete the last line of file
+sed -i '' -e "$ d" file.txt
